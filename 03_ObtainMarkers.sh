c4c1d9d88bbf768eac5e72e736588e24ac2e5d90
Fletcher <kfletcher@ucdavis.edu>
2020-05-05 10:36:03 -0700

    Fix JF awk filtering

--- 03_ObtainMarkers.sh
+++ 03_ObtainMarkers.sh
@@ -13,8 +13,8 @@ while getopts ':hM:1:2:W:p:' option; do
                          ;;
                 2)  Pare2=$OPTARG
                          ;;
-                W)  Mark2=$OPTARG
-                         ;;
+#                W)  Mark2=$OPTARG
+#                         ;;
 		p)  Out=$OPTARG
 			 ;;
                 \?) printf "illegal option: -%s\n\n" "$OPTARG" >&2
@@ -37,12 +37,15 @@ if [[ $AB =~ ^ABYSS ]]; then echo "$AB detected"; else echo "ABySS not detected,
 JF=$(jellyfish --version)
 if [[ $JF =~ ^jellyfish ]]; then echo "$JF detected"; else echo "jellyfish not detected, please modify your PATH" ; exit 1 ; fi
 
+#Could run below in a sub script to run both in parallel.
+#./GetMarkers.sh -M $Mark1 -1 $Pare1 -2 $Pare2 -p $Out
+#Would work well for providing the second with option W
 
 #1. Identify unique mers to P1.
-jellyfish query -s $Mark1 $Pare2 | awk '$1 == 0 {print ">"++i,"\n"$2}' > ./$t/${Out}Mark_P2zero.fa
+jellyfish query -s $Mark1 $Pare2 | awk '$2 == 0 {print ">"++i,"\n"$1}' > ./$t/${Out}Mark_P2zero.fa
 #First instance of STDERR will overwrite file if present.
 M1cou=$(grep -c '^>' ./$t/P1Mark_P2zero.fa)
-echo "$M1cou mers unique to $Pare1 when compared to $Pare2" >&2 03_AFLAP.stderr
+echo "$M1cou mers unique to $Pare1 when compared to $Pare2" >&2
 
 #2. Assemble unique
 ABYSS -k25 -c0 -e0 ./$t/P1Mark_P2zero.fa -o ./$t/${Out}Mark_ABYSS.fa
@@ -52,7 +55,7 @@ MMcou=$(cat ./$t/${Out}Mark_ABYSS.fa | paste - - | awk '$2 == 61' | wc -l)
 MMlen=$(cat ./$t/${Out}Mark_ABYSS.fa | paste - - | awk '$2 == 61{sum += $2}END{print sum}')
 MHcou=$(cat ./$t/${Out}Mark_ABYSS.fa | paste - - | awk '$2 > 61' | wc -l)
 MHlen=$(cat ./$t/${Out}Mark_ABYSS.fa | paste - - | awk '$2 > 61{sum += $2}END{print sum}')
-echo -e "$Mlcou fragments smaller than 61bp, totalling $MLlen bp\n$Mlcou fragments equal to 61bp, totalling $MMlen bp\n$MHcou fragments larger than 61bp, totalling $MHlen bp\n" >>&2 03_AFLAP.stderr
+echo -e "$Mlcou fragments smaller than 61bp, totalling $MLlen bp\n$Mlcou fragments equal to 61bp, totalling $MMlen bp\n$MHcou fragments larger than 61bp, totalling $MHlen bp\n" >&2
 
 #3. Filter Markers against hashes.
 cat ./$t/${Out}Mark_ABYSS.fa | paste - - | awk '$2 == 61 {print $1"\n"substr($4,10,31)}' > ./$t/${Out}_61bp.fa
@@ -61,12 +64,12 @@ cat ./$t/${Out}Mark_ABYSS.fa | paste - - | awk '$2 > 61 {print $1"\n"substr($4,1
 #Boundaries can be parsed from header.
 L=$(echo $Mark1 | sed 's/.*L//' | sed 's/_.*//')
 U=$(echo $Mark1 | sed 's/.*U//' | sed 's/\..*//')
-echo "Lower bound of $L obtained from file name" >>&2 03_AFLAP.stderr
-echo "Upper bound of $U obtained from file name" >>&2 03_AFLAP.stderr
-jellyfish query -s ./$t/${Out}_61bp.fa $Pare1 | awk -v Low="$L" -v Up="$U" '$1 >= Low && $2 <= Up {print ">"++i"\n"$2}' > ./$t/${out}_61bp_het.fa
-jellyfish query -s ./$t/${out}_61bp_het.fa $Pare2 | awk '$1 == 0 {print ">"++i"\n"$2}' > ./$t/${out}_61bp_MARKER.fa
-jellyfish query -s ./$t/${Out}_62bp.fa $Pare1 | awk -v Low="$L" -v Up="$U" '$1 >= Low && $2 <= Up {print ">"++i"\n"$2}' > ./$t/${out}_62bp_het.fa
-jellyfish query -s ./$t/${out}_62bp_het.fa $Pare2 | awk '$1 == 0 {print ">"++i"\n"$2}' > ./$t/${out}_62bp_MARKER.fa
+echo "Lower bound of $L obtained from file name" >&2
+echo "Upper bound of $U obtained from file name" >&2
+jellyfish query -s ./$t/${Out}_61bp.fa $Pare1 | awk -v Low="$L" -v Up="$U" '$2 >= Low && $2 <= Up {print ">"++i"\n"$2}' > ./$t/${out}_61bp_het.fa
+jellyfish query -s ./$t/${out}_61bp_het.fa $Pare2 | awk '$2 == 0 {print ">"++i"\n"$2}' > ./$t/${out}_61bp_MARKER.fa
+jellyfish query -s ./$t/${Out}_62bp.fa $Pare1 | awk -v Low="$L" -v Up="$U" '$2 >= Low && $2 <= Up {print ">"++i"\n"$2}' > ./$t/${out}_62bp_het.fa
+jellyfish query -s ./$t/${out}_62bp_het.fa $Pare2 | awk '$2 == 0 {print ">"++i"\n"$2}' > ./$t/${out}_62bp_MARKER.fa
 
 #4 Obtain ABYSS headers (Not currently essential)
 #Obtain RevCom of ABYSS
@@ -76,7 +79,7 @@ cat ./$t/${Out}_62bp.fa | paste - - | cut -f 2 | tr 'ATCG' 'TAGC' | rev | paste 
 cat ./$t/${Out}_62bp_MARKER.fa | paste - - | sort -k2,2 | join -1 2 -2 2 -o "1.1,0" - <(cat ./$t/${Out}_62bp_RC.fa | paste - -) | tr ' ' '\n' > ./${Out}_62bp_MARKER.fa
 M61=$(grep -c '^>' ./${Out}_61bp_MARKER.fa)
 M62=$(grep -c '^>' ./${Out}_62bp_MARKER.fa)
-echo -e "$M61 unique single copy markers equal to 61bp obtained from $Pare1 for downstream analysis" >>&2 03_AFLAP.stderr
-echo -e "$M62 unique single copy markers larger than 61 bp obtained from $Pare1 for downstream analysis" >>&2 03_AFLAP.stderr
+echo -e "$M61 unique single copy markers equal to 61bp obtained from $Pare1 for downstream analysis" >&2
+echo -e "$M62 unique single copy markers larger than 61 bp obtained from $Pare1 for downstream analysis" >&2
 
 exit
