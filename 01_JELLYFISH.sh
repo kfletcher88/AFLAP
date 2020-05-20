dae9207e64508efc63779cea6bf8349f40136d04
Kyle Fletcher <kfletcher@ucdavis.edu>
2020-05-20 13:07:13 -0700

    Run JELLYFISH on all F0, D'OH!

--- 01_JELLYFISH.sh
+++ 01_JELLYFISH.sh
@@ -8,7 +8,7 @@ while getopts ':hP:t:m:' option; do
                          ;;
                 P)  Ped=$OPTARG
                          ;;
-		t)  threads=$OPTARG
+		t)  thread=$OPTARG
 			;;
 		m)  mer=$OPTARG
 			;;
@@ -26,9 +26,9 @@ if [[ -z $Ped ]]; then
         exit 1
 fi
 #Set defaults if not specified.
-if [[ -z $threads ]]; then
+if [[ -z $thread ]]; then
 echo "Threads not specified, will proceed with default [4]"
-threads=4
+thread=4
 fi
 
 if [[ -z $mer ]]; then
@@ -36,9 +36,6 @@ echo "mer size not specified, will proceed with default [31]"
 mer=31
 fi
 
-#Set option for flexibility.
-mer=31
-thread=8
 echo "Beginning AFLAP"
 
 #0. Dependency Check
@@ -54,9 +51,9 @@ mkdir -p AFLAP_Intermediate
 #Check labels
 Flab=$(awk '$2 > 2' $Ped | wc -l)
 if [[ $Flab > 0 ]]; then echo "Pedigree file specifies individuals which are not F0, F1 ir F2. AFLAP has not been validated on this type of data so will terminate." ; exit 1 ; fi
-Ilab=$(awk '{print $1, $2' $Ped | sort | awk '{print $1}' | sort | uniq -c | awk '$2 > 1' | wc -l)
+Ilab=$(awk '{print $1, $2}' $Ped | sort -u | awk '{print $1}' | sort | uniq -c | awk '$1 > 1' | wc -l)
 if [[ $Ilab > 0 ]]; then echo "Pedigree file specifies the same individual label over multiple generations. This is incompatable with AFLAP so will terminate." 
-awk '{print $1, $2' $Ped | sort | awk '{print $1}' | sort | uniq -c | awk '$2 > 1' 
+awk '{print $1, $2}' $Ped | sort | awk '{print $1}' | sort | uniq -c | awk '$1 > 1'
 exit 1
 fi
 
@@ -67,6 +64,7 @@ Pcou=$(wc -l AFLAP_tmp/01/F0.txt | awk '{print $1}')
 if [[ $Pcou == 2 ]]
 then
 echo -e "$Pcou parents detected. Simple! Beginning k-mer counting"
+sort -uk1,1 $Ped | awk '$2 > 0 {print $2, $4, $5}' | sort | uniq -c > AFLAP_tmp/01/Crosses.txt
 elif [[ $Pcou > 2 ]]
 then
 echo -e "$Pcou parents detected. Not so simple! Determining crosses"
@@ -90,19 +88,19 @@ fi
 #Currently only tested on F1 and F2 independently. Has not been tested on both simultaneously.
 F1=$(awk '$2 == 1 {print $3, $4}' AFLAP_tmp/01/Crosses.txt | sort -u | wc -l)
 F2=$(awk '$2 == 2 {print $3, $4}' AFLAP_tmp/01/Crosses.txt | sort -u | wc -l)
-if [[ F1 > 0 && F2 > 0 ]]
+if [[ $F1 > 0 && $F2 > 0 ]]
 then
 echo "both F1 and F2 populations detected. AFLAP has not been validated on this type of data so will terminate. Please try to rerun specifying either type of population."
 exit 1
-elif [[ F1 > 0 && F2 == 0]]
+elif [[ $F1 > 0 && $F2 == 0 ]]
 then
 echo "$F1 F1 cross(es) identified."
-awk '$2 == 1 {print $3, $4}' AFLAP_tmp/01/Crosses.txt | sort -u | uniq -c > AFLAP_tmp/01/ParentsToCompare.txt
+awk '$2 == 1 {print $3 "x" $4}' AFLAP_tmp/01/Crosses.txt > AFLAP_tmp/01/ParentsToCompare.txt
 head AFLAP_tmp/01/ParentsToCompare.txt
-elif [[ F1 == 0 && F2 > 0]]
+elif [[ $F1 == 0 && $F2 > 0 ]]
 then
 echo "$F2 F2 cross(es) identified."
-awk '$2 == 1 {print $3, $4}' AFLAP_tmp/01/Crosses.txt | sort -u | uniq -c > AFLAP_tmp/01/ParentsToCompare.txt
+awk '$2 == 1 {print $3 "x" $4}' AFLAP_tmp/01/Crosses.txt > AFLAP_tmp/01/ParentsToCompare.txt
 head AFLAP_tmp/01/ParentsToCompare.txt
 fi
 
@@ -131,17 +129,17 @@ fi
 mkdir -p AFLAP_Intermediate/ParentalCounts
 PLA=$(cat AFLAP_tmp/01/LA.txt | wc -l)
 echo -e "Begining k-mer counting for $PLA parents"
-for g in `cat AFLAP_tmp/01/LA.txt` 
+for g in `cat AFLAP_tmp/01/F0.txt`
   do
 #Check if has has already been created
 	if [[ -e AFLAP_Intermediate/ParentalCounts/$g.jf${mer} ]]
 	then
 #If so, skip.
-	echo -e "$mer hash detected for $g. Skipping. Not correct, cancel and delete ParentalCounts/$g.fj${mer}, or run from clean directory"
+	echo -e "$mer hash detected for $g. Skipping. \n\tNot correct? Cancel and delete AFLAP_Intermediate/ParentalCounts/$g.fj${mer}, or run from clean directory"
 	else
 #If not, generate it.
 	echo -e "Begining k-mer counting for $g"
-	Reads=$(awk -v var="$g" '$1 == var {print $2}' $Ped | tr '\n' ' ')
+	Reads=$(awk -v var="$g" '$1 == var {print $3}' $Ped | tr '\n' ' ')
 	jellyfish count -s 1G -t $thread -m $mer -C -o AFLAP_Intermediate/ParentalCounts/$g.jf${mer} <(zcat $Reads)
 	fi
   done
@@ -149,21 +147,22 @@ echo -e "\nParental K-mer counting complete!\nOn to the progeny!\n"
 #Progeny.
 #Would be good to parallelize.
 mkdir -p AFLAP_Intermediate/ProgCounts
-awk '$2 != 0 {print $1}' | sort -u > AFLAP_tmp/01/Prog.txt
+awk '$2 != 0 {print $1}' $Ped | sort -u > AFLAP_tmp/01/Prog.txt
 ProgCou=$(cat AFLAP_tmp/01/Prog.txt | wc -l)
 echo -e "Begining k-mer counting for $ProgCou progeny"
 for g in `cat AFLAP_tmp/01/Prog.txt`
   do
 #Check if has has already been created
-        if [[ -e AFLAP_Intermediate/ProgenyCounts/$g.jf${mer} ]]
+        if [[ -e AFLAP_Intermediate/ProgCounts/$g.jf${mer} ]]
         then
 #If so, skip.
-        echo -e "$mer hash detected for $g. Skipping. Not correct, cancel and delete ParentalCounts/$g.fj${mer}, or run from clean directory"
+        echo -e "$mer hash detected for $g. Skipping. \n\tNot correct? Cancel and delete AFLAP_Intermediate/ProgCounts/$g.fj${mer}, or run from clean directory"
         else
 #If not, generate it.
 	echo -e "Begining k-mer counting for $g"
-	Reads=$(awk -v var="$g" '$1 == var {print $2}' $Ped | tr '\n' ' ')
+	Reads=$(awk -v var="$g" '$1 == var {print $3}' $Ped | tr '\n' ' ')
 	jellyfish count -s 1G -t $thread -m $mer -C -o AFLAP_Intermediate/ProgCounts/$g.jf${mer} <(zcat $Reads)
+	fi
   done
 echo "\nk-mer counting done!"
 exit
