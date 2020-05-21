4c48673fc40b36e2adfafda0b98dab94783e0a3c
Kyle Fletcher <kfletcher@ucdavis.edu>
2020-05-21 14:11:20 -0700

    Add HistoPlotting Rscript

---
+++ bin/HistoPlot.R
@@ -0,0 +1,8 @@
+#!/usr/bin/R
+library(ggplot2)
+args <- commandArgs(trailingOnly = TRUE)
+
+His <- read.table(args[1], sep=' ', header=FALSE)
+jpeg(args[2])
+ggplot(His, aes(x=V1,y=V2))+geom_line(colour="black")+coord_cartesian(xlim=c(0,250), ylim=c(0,4e6))+theme_minimal()+geom_vline(xintercept = c(as.numeric(args[3]), as.numeric(args[4])), linetype="dashed")+labs(x="Coverage", y="Frequency")
+dev.off()
