4c22d5afb4204654007eb4e9ffa474628feb82eb
Kyle Fletcher <kfletcher@ucdavis.edu>
2020-05-20 20:03:59 -0700

    Tested

--- 02_ExtractSingleCopyMers.sh
+++ 02_ExtractSingleCopyMers.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 
-while getopts ':hj:p:L:U:' option; do
+while getopts ':h:P:m:' option; do
         case "$option" in
                 h)  echo "$usage"
                          exit
                          ;;
                 P)  Ped=$OPTARG
                          ;;
-                m) mer=$OPTARG
+                m)  mer=$OPTARG
                          ;;
                 \?) printf "illegal option: -%s\n\n" "$OPTARG" >&2
                     echo "$usage"
@@ -41,17 +41,17 @@ fi
 
 #Check JF
 JF=$(jellyfish --version)
-if [[ $JF =~ ^jellyfish ]]; then echo "$JF detected" >&2 ; else echo "jellyfish not detected, please modify your PATH" >&2 ; exit 1 ; fi
+if [[ $JF =~ ^jellyfish ]]; then echo -e "\n$JF detected" >&2 ; else echo -e "\njellyfish not detected, please modify your PATH" >&2 ; exit 1 ; fi
 
 #Make new tmp for second script.
 mkdir -p AFLAP_tmp/02
 mkdir -p AFLAP_Intermediate/ParentalHisto
-echo "#Boundaries to follow" boundaries.txt
+echo "#Boundaries to follow" > AFLAP_tmp/02/Boundaries.txt
 #Obtain Histograms.
-echo -e "\nGenerating histograms for F0 to undergo linkage analysis"
+echo -e "\nGenerating histograms for F0 to undergo linkage analysis."
 for g in `cat AFLAP_tmp/01/LA.txt`
   do
-	if [[ -e AFLAP_Intermediate/ParentalHisto/$g.${mer}.histo ]]
+	if [[ -e AFLAP_Intermediate/ParentalHisto/$g.${mer}.histo ]] #1
 	then
 	#Use existing results.
 	echo -e "Histogram for $g ${mer}-mer detected. Will use those results.\nNot correct? Cancel and delete AFLAP_Intermediate/ParentalHisto/$g.${mer}.histo, or rerun in clean directory"
@@ -59,14 +59,14 @@ for g in `cat AFLAP_tmp/01/LA.txt`
 	#Generate histogram
 	jellyfish histo AFLAP_Intermediate/ParentalCounts/$g.jf${mer} > AFLAP_Intermediate/ParentalHisto/$g.${mer}.histo
 	echo -e "Histogram for $g generated"
-	fi
+	fi #1
   #Check to see if Coverage boundaries provided by the user?
-  BC=$(awk -v var="$g" '$1 == var && NF == 5 {print $1, $4, $5}' | head -n 1 | wc -l)
-	if [[ BC == 1 ]]
+  BC=$(awk -v var="$g" '$1 == var && NF == 5 {print $1, $4, $5}' $Ped | head -n 1 | wc -l)
+	if [[ $BC == 1 ]] #1
 	then
 	echo -e "User has supplied lower and upper boundaries for $g ${mer}-mer cut-offs.\nOn to extraction"
-	Lo=$(awk -v var="$g" '$1 == var && NF == 5 {print $4}' | head -n 1)
-	Up=$(awk -v var="$g" '$1 == var && NF == 5 {print $5}' | head -n 1)
+	Lo=$(awk -v var="$g" '$1 == var && NF == 5 {print $4}' $Ped | head -n 1)
+	Up=$(awk -v var="$g" '$1 == var && NF == 5 {print $5}' $Ped | head -n 1)
 	else
 	#Super hacky peak finding and boundary setting follows!
 	echo -e "User has not supplied boundaries for $g ${mer}-mer cut-offs. Automatic calculation underway\nNote results may not be accurate and plotted figures should be investigated.\n\tIf the F0 are under 20x coverage, reccomend run is cancelled and user defined boundaries be set"
@@ -80,20 +80,20 @@ for g in `cat AFLAP_tmp/01/LA.txt`
 	HomUp=$(printf %.0f $(echo $Peak*2.1 | bc -l))
 	HetLo=$(printf %.0f $(echo $Peak*0.4 | bc -l))
 	HetUp=$(printf %.0f $(echo $Peak*0.6 | bc -l))
-		if [[ $HomPeak > $HomLo && $HomTest < $HomUp ]]
+		if [[ $HomPeak > $HomLo && $HomTest < $HomUp ]] #2
 		then
 		echo "Additional peak detected at $HomPeak"
 		Hom=1
-		fi
-		if [[ $HetPeak > $HetLo && $HetPeak < $HetUp ]]
+		fi #2
+		if [[ $HetPeak > $HetLo && $HetPeak < $HetUp ]] #2
 		then
 		echo "Additional peak detected at $HetPeak"
 		Het=1
-		fi
-		if [[ $Hom == 1 && $Het != 1 ]]
+		fi #2
+		if [[ $Hom == 1 && $Het != 1 ]] #2
 		then
 		echo -e "\nHeterozygous peak =  $Peak\nHomozygous peak = $HomPeak"
-			if [[ $Cross == 1 ]]
+			if [[ $Cross == 1 ]] #3
 			then
 			echo "Analyzing F1 population so boundaries around the heterozygous peak will be used."
 			Lo=$(printf %.0f $(echo $Peak*0.75 | bc -l))
@@ -103,11 +103,11 @@ for g in `cat AFLAP_tmp/01/LA.txt`
 			echo "Analyzing F2 population so boundaries around the homozygous peak will be used."
 			Lo=$(printf %.0f $(echo $HomPeak*0.75 | bc -l))
                         Up=$(printf %.0f $(echo $HomPeak*1.5 | bc -l))
-			fi
+			fi #3
 		elif [[ $Hom != 1 && $Het == 1 ]]
 		then
 		echo -e "\nHeterozygous peak = $HetPeak\nHomozygous peak = $Peak"
-			if [[ $Cross == 1 ]]
+			if [[ $Cross == 1 ]] #3
                         then
                         echo "Analyzing F1 population so boundaries around the heterozygous peak will be used."
                         Lo=$(printf %.0f $(echo $HetPeak*0.75 | bc -l))
@@ -117,10 +117,11 @@ for g in `cat AFLAP_tmp/01/LA.txt`
                         echo "Analyzing F2 population so boundaries around the homozygous peak will be used."
                         Lo=$(printf %.0f $(echo $Peak*0.75 | bc -l))
                         Up=$(printf %.0f $(echo $Peak*1.5 | bc -l))
-                        fi
+                        fi #3
 		elif [[ $Hom == 1 && $Het == 1 ]]
+		then
 		echo -e "\nLooks like three peaks have been found. That's going to make things tricky!\nPeak 1 (Het) = $HetPeak\nPeak 2 (Het) = $Peak\nPeak 3 (Hom) = $HomPeak"
-			if [[ $Cross == 1 ]]
+			if [[ $Cross == 1 ]] #3
                         then
                         echo "Analyzing F1 population so boundaries around the two heterozygous peaks will be used."
                         Lo=$(printf %.0f $(echo $HetPeak*0.75 | bc -l))
@@ -130,11 +131,11 @@ for g in `cat AFLAP_tmp/01/LA.txt`
                         echo "Analyzing F2 population so boundaries around the homozygous peak will be used, however it seems like there is a large amount of heterozygosity"
                         Lo=$(printf %.0f $(echo $HomPeak*0.75 | bc -l))
                         Up=$(printf %.0f $(echo $HomPeak*1.5 | bc -l))
-                        fi
-		fi
-	echo "Lower boundary for $g set to $Lo, upper boundary to $Up"
-	echo -e "$g\t$Lo\t$Up" >> AFLAP_tmp/02/Boundaries.txt
-	fi
+                        fi #3
+		fi #2
+	fi #1
+  echo "Lower boundary for $g set to $Lo, upper boundary to $Up"
+  echo -e "$g\t$Lo\t$Up" >> AFLAP_tmp/02/Boundaries.txt
   if [[ -e AFLAP_Intermediate/ParentalHisto/${g}_m${mer}_L${Lo}_U${Up}.fa ]]
   then 
   echo -e "\n$g ${mer}-mer previously extracted between $Lo and $Up. Delete ${g}_m${mer}_L${Lo}_U${Up}.fa to rebuild." 
@@ -142,6 +143,7 @@ for g in `cat AFLAP_tmp/01/LA.txt`
   jellyfish dump -U $Up -L $Lo -o AFLAP_Intermediate/ParentalHisto/${g}_m${mer}_L${Lo}_U${Up}.fa AFLAP_Intermediate/ParentalCounts/$g.jf${mer}
   Kco=$(grep -c '^>'  AFLAP_Intermediate/ParentalHisto/${g}_m${mer}_L${Lo}_U${Up}.fa) 
   echo "$Kco ${mer}-mers extracted from $g"
-  Rscript bin/HistoPlot.R AFLAP_Intermediate/ParentalHisto/$g.${mer}.histo $Lo $Up AFLAP_Results/Plots/$g_m${mer}_L${Lo}_U${Up}_histo.png
+#  Rscript bin/HistoPlot.R AFLAP_Intermediate/ParentalHisto/$g.${mer}.histo $Lo $Up AFLAP_Results/Plots/$g_m${mer}_L${Lo}_U${Up}_histo.png
+  fi
   done
 exit
