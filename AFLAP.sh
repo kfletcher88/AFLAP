7b5da5fae97a76c0999a4a8f0e77b8a203a15723
Kyle Fletcher <kfletcher@ucdavis.edu>
2020-09-02 16:59:59 -0700

    Add Remove Functionality

--- AFLAP.sh
+++ AFLAP.sh
@@ -7,8 +7,9 @@ Options
         -P Pedigree file, required. See AFLAP README for more information.
         -m K-mer size. Optional. Default [31]
         -t Threads for JELLYFISH counting. Optional. Default [4]"
+	-r Individual to remove. All other options will be ignored.
 
-while getopts ':hP:t:m:' option; do
+while getopts ':hP:t:m:r:' option; do
         case "$option" in
                 h)  echo "$usage"
                          exit
@@ -19,6 +20,8 @@ while getopts ':hP:t:m:' option; do
                         ;;
                 m)  mer=$OPTARG
                         ;;
+		r)  rem=$OPTARG
+			;;
                 \?) printf "illegal option: -%s\n\n" "$OPTARG" >&2
                     echo "$usage"
                     exit 1
@@ -26,6 +29,7 @@ while getopts ':hP:t:m:' option; do
         esac
 done
 
+if [[ -z $rem ]]; then
 DIR=$(dirname $0)
 #00 Dependency check
 echo -e "\n\e[31mBeginning Step 1/6\e[0m" &&
@@ -40,5 +44,10 @@ echo -e "\n\e[31mBeginning Step 5/6\e[0m" &&
 $DIR/bin/05_ObtainSegStats.sh -P $Ped -m $mer &&
 echo -e "\n\e[31mBeginning Step 6/6\e[0m" &&
 $DIR/bin/06_ExportToLepMap3.sh -P $Ped -m $mer &&
+else
+echo -e "Removing intermediate progeny files generated for $rem"
+find AFLAP_Intermediate/ -name "${rem}*" -exec rm {} +
+find AFLAP_tmp/ -name "${rem}*" -exec rm {} +
+fi
 exit
 #Add additional scripts for running LepMap3
