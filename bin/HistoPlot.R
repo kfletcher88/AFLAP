064cceb58381527822fcd16a50312ebd84cdcd85
Kyle Fletcher <kfletcher@ucdavis.edu>
2020-09-02 14:44:02 -0700

    AddCorrectVlineOrder

--- bin/HistoPlot.R
+++ bin/HistoPlot.R
@@ -3,6 +3,6 @@ library(ggplot2)
 args <- commandArgs(trailingOnly = TRUE)
 
 His <- read.table(args[1], sep=' ', header=FALSE)
-jpeg(args[2])
-ggplot(His, aes(x=V1,y=V2))+geom_line(colour="black")+coord_cartesian(xlim=c(0,250), ylim=c(0,4e6))+theme_minimal()+geom_vline(xintercept = c(as.numeric(args[3]), as.numeric(args[4])), linetype="dashed")+labs(x="Coverage", y="Frequency")
+jpeg(args[4])
+ggplot(His, aes(x=V1,y=V2))+geom_line(colour="black")+coord_cartesian(xlim=c(0,250), ylim=c(0,4e6))+theme_minimal()+geom_vline(xintercept = c(as.numeric(args[3]), as.numeric(args[2])), linetype="dashed")+labs(x="Coverage", y="Frequency")
 null <- dev.off()
