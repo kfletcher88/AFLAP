004679071927b5f275a13be0be09640f61256c15
Kyle Fletcher <kfletcher@ucdavis.edu>
2020-09-02 14:54:37 -0700

    PreventLabelOverlapsUntested

--- bin/KmerCovXMarkerCount.R
+++ bin/KmerCovXMarkerCount.R
@@ -7,7 +7,7 @@ dat <- read.table(args[1], sep='\t', header=FALSE)
 png(args[2],width=600,height=600)
 ggplot(dat, aes(x=V3,y=V2,label=V1))+
 geom_point()+
-geom_text_repel()+
+geom_text_repel(check_overlap=TRUE)+
 theme_minimal()+
 labs(x="K-mer coverage", y="Marker Count")
 null <- dev.off()
