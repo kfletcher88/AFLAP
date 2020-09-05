2f1a650a3edfa4aca19b9dbe058c106744c9aa6b
Kyle Fletcher <kfletcher@ucdavis.edu>
2020-09-04 17:44:03 -0700

    Surpress overplotting

--- bin/KmerCovXMarkerCount.R
+++ bin/KmerCovXMarkerCount.R
@@ -3,11 +3,12 @@ library(ggrepel)
 args <- commandArgs(trailingOnly = TRUE)
 
 dat <- read.table(args[1], sep='\t', header=FALSE)
-
+dat$label <- dat$V1
+dat$label[dat$V3 > 3] <- NA
 png(args[2],width=600,height=600)
-ggplot(dat, aes(x=V3,y=V2,label=V1))+
+ggplot(dat, aes(x=V3,y=V2,label=label))+
 geom_point()+
-geom_text_repel(check_overlap=TRUE)+
+geom_text_repel(na.rm=TRUE)+
 theme_minimal()+
 labs(x="K-mer coverage", y="Marker Count")
 null <- dev.off()
