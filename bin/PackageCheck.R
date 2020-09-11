152b0cf3bdbeaa7a2f87d9e2a7cd261f83752d93
Kyle Fletcher <kfletcher@ucdavis.edu>
2020-09-11 11:30:38 -0700

    Add message to confirm packages found

--- bin/PackageCheck.R
+++ bin/PackageCheck.R
@@ -1,3 +1,7 @@
 #!/bin/R
 packages <- c("ggplot2","ggrepel")
-if (length(setdiff(packages, rownames(installed.packages())))>0){write("One or more packages missing, please check ggplot2 and ggrepel are both installed", stderr())}
+if (length(setdiff(packages, rownames(installed.packages())))>0){
+write("One or more packages missing, please check ggplot2 and ggrepel are both installed", stderr())
+} else {
+write("All necessary packages found. Good to run AFLAP!", stderr())
+}
