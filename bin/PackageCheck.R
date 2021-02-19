430bf14779ee0884bc1e74ecc05725cbedd28616
Kyle Fletcher <kfletcher@ucdavis.edu>
2021-02-19 11:22:02 -0800

    UpdatePackageCheck

--- bin/PackageCheck.R
+++ bin/PackageCheck.R
@@ -1,7 +1,7 @@
 #!/bin/R
-packages <- c("ggplot2","ggrepel")
+packages <- c("ggplot2","ggrepel", "dplyr")
 if (length(setdiff(packages, rownames(installed.packages())))>0){
-write("One or more packages missing, please check ggplot2 and ggrepel are both installed", stderr())
+write("One or more packages missing, please check ggplot2, ggrepel and dplyr are all installed", stderr())
 } else {
 write("All necessary packages found. Good to run AFLAP!", stderr())
 }
